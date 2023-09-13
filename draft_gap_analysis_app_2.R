649f1a7591ac70598079830a6d446bc63d7f4c18
Jonathan-Gore <43797248+Jonathan-Gore@users.noreply.github.com>
2023-09-13 13:56:24 -0400

    Added functionality to draw x,y points on map
    Added functionality to draw x,y points on map (Assuming wg84 and other "common" GIS standard.

---
+++ draft_gap_analysis_app_2.R
@@ -0,0 +1,134 @@
+library(shiny)
+library(tidyverse)
+library(rsconnect)
+library(leaflet) # added leaflet for map visualization
+
+tt <- tibble(AA=seq(10), BB=seq(10)*2, CC=seq(10)*3 )
+
+write_csv(tt, "test_input_file.csv")
+
+rm(tt)
+
+# Define UI for data upload app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("ABG Gap Analysis Tool"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Select a file ----
+      fileInput("file1", "Choose CSV File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv", "space")),
+      
+      # Horizontal line ----
+      tags$hr(),
+      
+      # Input: Checkbox if file has header ----
+      checkboxInput("header", "Header", TRUE),
+      
+      # Input: Select separator ----
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t",
+                               Space = " "),
+                   selected = ","),
+      
+      # Input: Select quotes ----
+      radioButtons("quote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"'),
+      
+      # Horizontal line ----
+      tags$hr(),
+      
+      # Input: Select number of rows to display ----
+      radioButtons("disp", "Display",
+                   choices = c(Head = "head",
+                               All = "all"),
+                   selected = "head"),
+      
+      # Add "Run Analysis" button ----
+      actionButton("runAnalysis", "Run Analysis")
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Data file ----
+      tableOutput("contents"),
+      
+      # Output: Display map ----
+      leafletOutput("map") # added this output for map
+      
+    )
+    
+  )
+)
+
+server <- function(input, output) {
+  
+  df = reactive({
+    req(input$file1)
+    
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    
+    return(df)
+  })
+  
+  output$contents <- renderTable({
+    
+    req(input$file1)
+    
+    tryCatch(
+      {
+        df <- read.csv(input$file1$datapath,
+                       header = input$header,
+                       sep = input$sep,
+                       quote = input$quote)
+      },
+      error = function(e) {
+        stop(safeError(e))
+      }
+    )
+    
+    if(input$disp == "head") {
+      return(head(df))
+    }
+    else {
+      return(df)
+    }
+    
+  })
+  
+  # Added code to display map
+  output$map <- renderLeaflet({
+    req(input$runAnalysis) # Ensure "Run Analysis" button is clicked
+    df_temp <- df()
+    
+    # Check if df_temp has columns "x" and "y"
+    if(all(c("x", "y") %in% colnames(df_temp))) {
+      leaflet(df_temp) %>%
+        addTiles() %>%
+        addCircleMarkers(~x, ~y, popup = ~paste(x, y))
+    }
+  })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
