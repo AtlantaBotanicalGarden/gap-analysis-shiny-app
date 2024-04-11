0412a254491f541eb1d2b3a41f24c94d511ecf64
dcarver1 <carver.dan1@gmail.com>
2024-04-11 10:03:09 -0700

    some minor changes during a meeting

--- app.R
+++ app.R
@@ -18,6 +18,7 @@ library(spocc)
 library(jsonlite)
 library(rgbif)
 library(stringr)
+library(shinycssloaders)
 # source modules --------------------------------------------------------
 lapply(list.files(
   path = "modules/",
@@ -44,12 +45,19 @@ valid_data_structure <- JSONdata[[1]]
 #   dplyr::filter(taxon=="Magnolia fraseri")
 
 # 
+# move this to a function !!!
 map1 <- leaflet::leaflet(options = leafletOptions(minZoom = 3, maxZoom = 16))|>
-  addTiles()
+  addTiles()|> 
+  # layer control groups should not be set at the map proxy level as they will overwrite the existing element.
+  addLayersControl( 
+    position = "topleft",
+    overlayGroups = c("GBIF", "Upload Dataset"),
+    options = layersControlOptions(collapsed = TRUE)
+  )
 # 
 map2 <- map1
 
-# expectedNames <- c("UID",	"taxon",	"genus",	"species",	"type",	"year",	"latitude",	"longitude",	"locality",	"collector")
+expectedNames <- c("UID",	"taxon",	"genus",	"species",	"type",	"year",	"latitude",	"longitude",	"locality",	"collector")
 
 
 
@@ -61,8 +69,8 @@ ui <- fluidPage(
   theme = bslib::bs_theme(
     version = "5",
     bootswatch = "yeti",
-    bg = "#FFFFFF",
-    fg = "#655560",
+    # bg = "#948",
+    # fg = "#655560",
     primary = "#52b788",
     secondary = "#f6bd60",
     base_font = font_google("Nunito Sans"),
@@ -210,7 +218,9 @@ ui <- fluidPage(
                         label = tags$a(href = "https://docs.google.com/spreadsheets/d/1BeDUBCg2BJ1DYpW47bxYH8p8CJAVqgM5rafyz81RoOc/edit#gid=1735583299",
                                        "View GBIF Issue Codes ", target = "_blank"), "see link below for options"),
               tags$br(),
-              tags$strong("Download Details:"),  textOutput("gbifDownloadSpecifics"),
+              # shinycssloaders::withSpinner(
+                tags$strong("Download Details:"),  textOutput("gbifDownloadSpecifics"),
+              # ),
               tags$br(),
               actionButton("gbifToMap", "Add GBIF to map")
             ),
@@ -224,10 +234,15 @@ ui <- fluidPage(
             ),
             accordion_panel(
               "4. Remove Data from Map ",
-              "Eventually this might expand to have some more data cleaning options.",
-              actionButton("pointsToMap", "Remove all GBIF data"),
-              actionButton("pointsToMap", "Remove uploaded data")
+              "Selecting a button will remove a specific layer from the map. The toggle buttons in the top left corner of the map can be uses to hide specific layers.",
+              actionButton("removeGBIF", "Remove all GBIF data"),
+              actionButton("removeUpload", "Remove uploaded data")
             ),
+            accordion_panel(
+              "5. Move data to Gap Analysis",
+              "Select the botton below with gather all GBIF and uploaded datasets and add them to the Gap Analysis Map.",
+              actionButton("compileDatasets", "Compile Data For Gap Analysis")
+              ),
             
             
           )
@@ -239,16 +254,29 @@ ui <- fluidPage(
           card_footer("Description of the map? ")
         )
       ),
-    card(
-      card_header("Information on the Records"),
+    navset_card_tab(
+        height = 450,
+        full_screen = TRUE,
+        title = "Tabular Data of the Records",
+        nav_panel(
+          "GBIF_table",
+          downloadButton("download1", "Download the GBIF data"),
+          card_title("GBIF Records"),
+          DTOutput("mapTableGBIF"),
+        ),
+        nav_panel(
+          "Uploaded Data",
+          downloadButton("download2", "Download the Uploaded data"),
+          card_title("Uploaded Records"),
+          DTOutput("mapTableUpload"),
+        )
+      )
       ### add a tab section here for evaluating GBIF or uploaded record 
-      tags$p("**note**: currently this auto populates once a genus species is selected. Once we transistion to uploading data or grabing data from gbif and uploading users will have to 
-             use a button to add content to this table"),
-      DTOutput("mapTable"),
-      downloadButton("download1", "Download the current table")
-    )
-    
-  ), ## end data analysis page
+      # tags$p("**note**: currently this auto populates once a genus species is selected. Once we transistion to uploading data or grabing data from gbif and uploading users will have to 
+      #        use a button to add content to this table"),
+      # DTOutput("mapTable"),
+      # downloadButton("download1", "Download the current table")
+    ), ## end data analysis page
 
   # Gap Analysis ------------------------------------------------------------
   nav_panel(
@@ -265,7 +293,7 @@ ui <- fluidPage(
     ),
     layout_sidebar(
       # sidebar feature
-      height="600px", # does not seem to actively effect this 
+      height="600px", # does not seem to actively effect this
       sidebar = sidebar(
         position = "right",
         accordion(
@@ -294,7 +322,7 @@ ui <- fluidPage(
       card_header("Results of the Gap Analysis"),
       DTOutput("mapTable2"),
       card_footer(
-        actionButton("exportGapTabke", "Download the current table")
+        actionButton("exportGapTable", "Download the current table")
       )
     )
   ),## end gap analysis page
@@ -360,6 +388,7 @@ server <- function(input, output) {
   })
   
   # pull the gbif id from the selection 
+  ### might be nice to have a single generic function that takes input parameters for all selections. 
   output$gbiftaxonid <- renderText({
     f1 <- gbifBackbone |>
       dplyr::filter(genus == as.character(input$genusSelect)) |>
@@ -378,25 +407,26 @@ server <- function(input, output) {
       
   })
 
-  # generate the GBIF table data 
-  # df1 <- reactive({
-  #   f1 <- gbifData() |>
-  #     dplyr::filter(genus == as.character(input$genusSelect)) |>
-  #     dplyr::filter(specificEpithet == as.character(input$speciesSelect))
-  #   
-  #   if(as.character(input$taxonRank) == "species"){
-  #     f1
-  #   }else{
-  #     f1 |> 
-  #       dplyr::filter(taxonRank == as.character(input$taxonRank))|>
-  #       dplyr::filter(taxonRank == as.character(input$speciesIntrfraspecific))
-  #   }
-  # })
-
   # Download data from GBIF -------------------------------------------------
   gbifData <- eventReactive(input$gbifPull, {
+    ### repeated method from print render 
+    f1 <- gbifBackbone |>
+      dplyr::filter(genus == as.character(input$genusSelect)) |>
+      dplyr::filter(specificEpithet == as.character(input$speciesSelect))
+    
+    if(as.character(input$taxonRank) == "species"){
+      f2 <- f1$taxonID[1] # issue 
+    }else{
+      f2 <- f1 |> 
+        dplyr::filter(taxonRank == as.character(input$taxonRank))|>
+        dplyr::filter(taxonRank == as.character(input$speciesIntrfraspecific))|>
+        dplyr::select(taxonID)|>
+        dplyr::pull()
+      
+    }
+    
     # define params 
-    initialPull <- query_gbif_occ(taxonkey = 2878373,
+    initialPull <- query_gbif_occ(taxonkey = f2,
                    allow_synonyms_bool = TRUE)
     
     # filter to specific layers 
@@ -405,12 +435,13 @@ server <- function(input, output) {
     
     structureData
   })
+  
   # 
   output$gbifDownloadSpecifics <- renderText({
-    if(is.null(gbifData)){
+    if(is.null(gbifData())){
       "There is no data available on GBIF for this species"
     }else{
-      paste0("The query returned ", nrow(gbifData), " records.")
+      paste0("The query returned ", nrow(gbifData()), " records.")
     }
   })
 
@@ -434,7 +465,6 @@ server <- function(input, output) {
     # update the map
     leafletProxy("map1")|>
       setView(lng = pointsVals$longitude[1], lat = pointsVals$latitude[1], zoom = 6)|>
-      clearGroup(group = "GBIF") |>
       addCircleMarkers(
         data = pointsVals,
         color = ~color,
@@ -446,21 +476,17 @@ server <- function(input, output) {
       # single legend for the GBIF features
       addLegend(
         position = "topright",
+        layerId = "GBIFlegend",
         colors = c("#1184d4","#6300f0"),
         labels = c("H","G"),
         title = "GBIF Data",
         opacity = 1,
         group = "GBIF"
-      )|>
-      addLayersControl(
-        position = "topleft",
-        overlayGroups = c("GBIF"),
-        options = layersControlOptions(collapsed = FALSE)
       )
   })
   
   # create spatial data for uploaded data  -----------------------------------
-  sp2 <- observeEvent(input$uploadToMap, {
+ observeEvent(input$uploadToMap, {
     # generate spatial object 
     pointsVals2 <- dataUpload()|>
       sf::st_as_sf(coords = c("longitude","latitude"), crs = 4326, remove = FALSE)|>
@@ -474,12 +500,25 @@ server <- function(input, output) {
           type == "G" ~ "#6300f0"
         )
       )
+    # spilt out the G and H points 
+    gVals2 <- pointsVals2 |>
+      dplyr::filter(type == "G")
+    hVals2 <- pointsVals2 |>
+      dplyr::filter(type == "H")
     # update the map 
     leafletProxy("map1")|>
       setView(lng = pointsVals2$longitude[1], lat = pointsVals2$latitude[1], zoom = 6)|>
       clearGroup(group = "Upload Dataset") |>
       addCircleMarkers(
-        data = pointsVals2,
+        data = hVals2,
+        color = ~color,
+        stroke = TRUE,
+        fillOpacity = 0.9,
+        popup = ~popup,
+        group = "Upload Dataset"
+      ) |>
+      addCircleMarkers(
+        data = gVals2,
         color = ~color,
         stroke = TRUE,
         fillOpacity = 0.9,
@@ -489,23 +528,34 @@ server <- function(input, output) {
       # single legend for the GBIF features
       addLegend(
         position = "topright",
+        layerId = "uploadlegend",
         colors = c("#1184d4","#6300f0"),
         labels = c("H","G"),
         title = "Upload Dataset",
         opacity = 1,
         group = "Upload Dataset"
-      )|>
-      addLayersControl(
-        position = "topleft",
-        overlayGroups = c("Upload Dataset"),
-        options = layersControlOptions(collapsed = FALSE)
-      )
+      ) 
   })
   
-  
+
+  # Remove layers from map  -------------------------------------------------
+  ## GBIF 
+  observeEvent(input$removeGBIF, {
+    leafletProxy("map1")|>
+      clearGroup(group = "GBIF")|>
+      removeControl(layerId = "GBIFlegend") # doesn't seem to be working at the moment. Mugh
+      # not sure if I can remove the layer control element once it is added 
+  })
+  ## upload data
+  observeEvent(input$removeUpload, {
+    leafletProxy("map1")|>
+      clearGroup(group = "Upload Dataset")|>
+      removeControl(layerId = "uploadlegend") # doesn't seem to be working at the moment. Mugh
+    # not sure if I can remove the layer control element once it is added 
+  })
 
   # Export the table from data processing page  -----------------------------------------------
-  # The requested dataset
+  # GBIF Data
   output$download1 <- downloadHandler(
     filename = function() {
       # Use the selected dataset as the suggested file name
@@ -513,9 +563,21 @@ server <- function(input, output) {
     },
     content = function(file) {
       # Write the dataset to the `file` that will be downloaded
-      write.csv(df1(), file, row.names = FALSE)
+      write.csv(gbifData(), file, row.names = FALSE)
     }
   )
+  
+  output$download2 <- downloadHandler(
+    filename = function() {
+      # Use the selected dataset as the suggested file name
+      paste0(input$genusSelect,"_",input$speciesSelect, "_data.csv")
+    },
+    content = function(file) {
+      # Write the dataset to the `file` that will be downloaded
+      write.csv(dataUpload(), file, row.names = FALSE)
+    }
+  )
+  
   
 
   # read in the uploaded data -----------------------------------------------
@@ -547,13 +609,35 @@ server <- function(input, output) {
     
     
   })
+
+  # compile dataset for the gap analysis page  ------------------------------
+  mapData <- data.frame(matrix(nrow = 1, ncol = length(expectedNames)+1))
+  names(mapData) <- c(expectedNames, "source")
   
+  gapAnalysisInput <- eventReactive(input$compileDatasets, {
+    # test for presences of data
+    if(!is.null(gbifData())){
+      d1 <- gbifData()|>
+        dplyr::mutate(source = "GBIF")
+      mapData <- bind_rows(mapData, d1) # I don't think this is going to work super well but let's run with it. 
+    }
+    #upload 
+    if(!is.null(gbifData())){
+      d2 <- dataUpload()|>
+        dplyr::mutate(source = "upload")
+      mapData <- bind_rows(mapData, d2)
+    }
+    mapData
+  })
   
+  # 
   output$map1 <- leaflet::renderLeaflet(map1)
-  output$mapTable <- renderDT(gbifData())
+  output$mapTableGBIF <- renderDT(gbifData())
+  output$mapTableUpload <- renderDT(dataUpload())
+  
   output$image <- renderImage(random_image())
   output$map2<- leaflet::renderLeaflet(map2)
-  output$mapTable2 <- renderDT(shinipsum::random_DT(nrow = 5,ncol = 4))
+  output$mapTable2 <- renderDT(gapAnalysisInput())
 
 }
 
--- appFunctions/gbif_query.r
+++ appFunctions/gbif_query.r
@@ -8,67 +8,38 @@ library("rgbif")
 library("dplyr")
 library("stringr")
 
-
-## Core function ##
-query_gbif_taxonomy <- function(qstring, qrank) {
-  api_result <- name_suggest(q = qstring, rank = qrank, limit = 100)
-  taxon_df <- api_result$data
-  taxon_df <- taxon_df[taxon_df$canonicalName == qstring,]
-  return (taxon_df$key)
-}
-
-## Elaborated Function to handle pagination ##
-fuzzy_search_gbif_taxonomy <- function(qstring, qrank) {
-  limit <- 100 # The max number of records per page
-  start <- 0   # Start at the beginning
-  all_data <- tibble() # Initialize an empty tibble to store results
-  
-  repeat {
-    # Fetch a page of results
-    api_result <- name_suggest(q = qstring, rank = qrank, limit = limit, start = start)
-    # Combine with previous results
-    all_data <- bind_rows(all_data, api_result$data)
-    
-    # Check if there are more results to fetch
-    if (nrow(api_result$data) < limit) {
-      break # Exit loop if we've fetched all results
-    }
-    
-    # Otherwise, prepare to fetch the next page
-    start <- start + limit
-  }
-  
-  taxon_df <- api_result$data
-  taxon_df <- taxon_df[taxon_df$canonicalName == qstring,]
-  
-  # Return unique results
-  return(taxon_df)
-}
-
-## Query Children of GBIF Taxon Key ##
-# Current arbitrary limit set to 10000 because it lets me
-# could implement pagination in future if required if a taxa has more than 10000 children
-query_gbif_taxon_key_child <- function(qkey, qgenus, qrank) {
-  
-  api_result <- name_usage(qkey, data = "children", rank = qrank, limit = 10000)
-  
-  childdata <- api_result$data
-  childdata <- childdata[childdata$taxonomicStatus == "ACCEPTED",]
-  childdata <- filter(childdata, sapply(str_split(childdata$species, " "), function(x) x[1]) == qgenus)
-  
-  return(childdata)
-}
+# used functions ----------------------------------------------------------
 
 
 ## Getting GBIF Occurrence Data ##
 # Allows for optional filtering out multiple issues
 # reference issue codes can be found in issues.csv [https://github.com/AtlantaBotanicalGarden/gap-analysis-shiny-app/issues/2]
 query_gbif_occ <- function(taxonkey, allow_synonyms_bool = TRUE, issues_not_allowed = NULL) {
-  gbif_response <- occ_data(taxonKey = taxonkey,
+  # call all G points 
+  gbif_response1 <- rgbif::occ_data(taxonKey = taxonkey,
                             hasCoordinate = TRUE,
-                            hasGeospatialIssue = FALSE)
-
-  parsed_response <- gbif_response$data
+                            hasGeospatialIssue = FALSE,
+                            basisOfRecord = "LIVING_SPECIMEN"
+                            )
+  # Set the total number to be less then or equal to 500
+  if(is.null(nrow(gbif_response1$data))){
+    pull <- 500
+  }else{
+    pull <- 500 - nrow(gbif_response1$data)
+  }
+  # call H points with the amount 
+  gbif_response2 <- rgbif::occ_data(taxonKey = taxonkey,
+                                   hasCoordinate = TRUE,
+                                   hasGeospatialIssue = FALSE,
+                                   limit = pull)
+  # bind the datasets
+  if(is.null(nrow(gbif_response1$data))){
+    parsed_response <- gbif_response2
+  }else{
+    parsed_response <- dplyr::bind_rows(gbif_response1$data, gbif_response2$data)
+  }
+  
+  # parsed_response <- gbif_response$data
   pattern <- paste(issues_not_allowed, collapse = "|")
   
   if(pattern != ""){
@@ -78,7 +49,62 @@ query_gbif_occ <- function(taxonkey, allow_synonyms_bool = TRUE, issues_not_allo
   if(allow_synonyms_bool == FALSE){
     parsed_response <- parsed_response[parsed_response$taxonomicStatus == "ACCEPTED",]
   }
-  
-  return(parsed_response)
+  parsed_response
 }
+
+
+# currently not used ------------------------------------------------------
+
+# ## Core function ##
+# query_gbif_taxonomy <- function(qstring, qrank) {
+#   api_result <- name_suggest(q = qstring, rank = qrank, limit = 100)
+#   taxon_df <- api_result$data
+#   taxon_df <- taxon_df[taxon_df$canonicalName == qstring,]
+#   return (taxon_df$key)
+# }
+# 
+# ## Elaborated Function to handle pagination ##
+# fuzzy_search_gbif_taxonomy <- function(qstring, qrank) {
+#   limit <- 100 # The max number of records per page
+#   start <- 0   # Start at the beginning
+#   all_data <- tibble() # Initialize an empty tibble to store results
+#   
+#   repeat {
+#     # Fetch a page of results
+#     api_result <- name_suggest(q = qstring, rank = qrank, limit = limit, start = start)
+#     # Combine with previous results
+#     all_data <- bind_rows(all_data, api_result$data)
+#     
+#     # Check if there are more results to fetch
+#     if (nrow(api_result$data) < limit) {
+#       break # Exit loop if we've fetched all results
+#     }
+#     
+#     # Otherwise, prepare to fetch the next page
+#     start <- start + limit
+#   }
+#   
+#   taxon_df <- api_result$data
+#   taxon_df <- taxon_df[taxon_df$canonicalName == qstring,]
+#   
+#   # Return unique results
+#   return(taxon_df)
+# }
+# 
+# ## Query Children of GBIF Taxon Key ##
+# # Current arbitrary limit set to 10000 because it lets me
+# # could implement pagination in future if required if a taxa has more than 10000 children
+# query_gbif_taxon_key_child <- function(qkey, qgenus, qrank) {
+#   
+#   api_result <- name_usage(qkey, data = "children", rank = qrank, limit = 10000)
+#   
+#   childdata <- api_result$data
+#   childdata <- childdata[childdata$taxonomicStatus == "ACCEPTED",]
+#   childdata <- filter(childdata, sapply(str_split(childdata$species, " "), function(x) x[1]) == qgenus)
+#   
+#   return(childdata)
+# }
+
+
+
 
